fbb10ca77952d19c543a7c61555b818fa3523b49
Richard Messerly <richard.messerly@nist.gov>
2018-06-06 14:57:44 -0600

    Added some simulation results. Updated Rerun_GK.

--- Scripts/Rerun_Green_Kubo_Analyze.sh
+++ Scripts/Rerun_Green_Kubo_Analyze.sh
@@ -2,13 +2,20 @@
 # This small script automates rerunning the Green Kubo analysis for
 # however many rhos are needed. 
 # Specify the output path by hand.
-Compound=C4H10
+Compound=IC8H18
+Model=TAMie
+Conditions_type=Saturation
+Nmol=400
 rho_low=0  # First rho to be rerun
-rho_high=0  # Second rho to be rerun
+rho_high=4  # Last rho to be rerun
 low_lim=0  # Low limit of number of replicates to pass to GK
-high_lim=3  # High limit of number of replicates to pass to GK
-scripts_path=/Scripts  # Where to find GK
-output_path="$PWD"  # Where to find the data
+high_lim=59  # High limit of number of replicates to pass to GK
+scripts_path=~/Scripts  # Where to find GreenKubo_analyze
+# Where to find the data
+output_path=~/"$Compound"/Gromacs/"$Conditions_type"_Viscosity/"$Model"_N"$Nmol" #
+#output_path="$PWD" 
+#output_path=~/"$Compound"/Gromacs/"$Model"_N"$Nmol"
+#output_path=~/"$Compound"/Gromacs/"$Model"
 
 cd "$output_path" || exit 1  # Enter analysis directory or quit
 
