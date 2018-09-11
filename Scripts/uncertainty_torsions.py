627e07d0ebb0b65a3026ff91f90871a6dab4fb6a
Richard Messerly <richard.messerly@nist.gov>
2018-09-11 08:58:26 -0600

    Updated some input files.

--- Scripts/uncertainty_torsions.py
+++ Scripts/uncertainty_torsions.py
@@ -7,6 +7,7 @@ Class for quantifying uncertainty in torsional parameters
 from __future__ import division
 import numpy as np 
 import matplotlib.pyplot as plt
+from scipy.stats import skewnorm
 
 kb=1.38064852*10.**(-23.) #[J/K]
 R_g = 8.3144598 / 1000. #[kJ/mol/K]
@@ -54,6 +55,7 @@ def U_tors_form2(phi,a_tors):
 a_TraPPE_CH2_CH2 = np.array([0.0,355.03,-68.19,791.32])
 a_TraPPE_CH2_CH = np.array([-251.06,428.73,-111.85,441.27])
 a_TraPPE_CH2_C = np.array([0.0,0.0,0.0,461.29])
+a_TraPPE_CH_CH = a_TraPPE_CH2_CH.copy()
 
 U_plot_form2 = lambda a_tors: U_tors_form2(phi_plot,a_tors)
 
@@ -120,19 +122,122 @@ plt.legend()
 plt.show()
 
 STD_CH2_CH2 = 0.15 * np.max(U_TraPPE_CH2_CH2) / 1.96
-STD_CH2_CH = 0.15 * np.max(U_TraPPE_CH2_CH) / 1.96
-STD_CH2_C = 0.15 * np.max(U_TraPPE_CH2_C) / 1.96                           
+STD_CH2_CH = -0.15 * np.max(U_TraPPE_CH2_CH) / 1.96
+STD_CH2_C = -0.15 * np.max(U_TraPPE_CH2_C) / 1.96                           
                          
-N_MCMC = 20                         
+N_MCMC = 2000                        
                          
-MCMC_CH2_CH2 = np.random.normal(0,STD_CH2_CH2,N_MCMC)
-MCMC_CH2_CH = np.random.normal(0,STD_CH2_CH,N_MCMC)
-MCMC_CH2_C = np.random.normal(0,STD_CH2_C,N_MCMC)                        
+### Original way of generating normal data
+#MCMC_CH2_CH2 = np.random.normal(0,STD_CH2_CH2,N_MCMC)
+#MCMC_CH2_CH = np.random.normal(0,STD_CH2_CH,N_MCMC)
+#MCMC_CH2_C = np.random.normal(0,STD_CH2_C,N_MCMC)   
 
-plt.hist(MCMC_CH2_CH2,bins=50,color='k')
-plt.hist(MCMC_CH2_CH,bins=50,color='r')
-plt.hist(MCMC_CH2_C,bins=50,color='b')
+### To generate a file with just shifted values, not sampled values
+#MCMC_CH2_CH2 = STD_CH2_CH2 * 1.96 * np.ones(N_MCMC)
+#MCMC_CH2_CH = STD_CH2_CH * 1.96 * np.ones(N_MCMC)
+#MCMC_CH2_C = STD_CH2_C * 1.96 * np.ones(N_MCMC) 
+#MCMC_CH_CH = MCMC_CH2_CH.copy()                    
+
+### To generate normal data
+#skew = .0
+#loc = 0.
+#### 15%
+#upperU = 0.15
+#### 40%
+#upperU = 0.4
+
+### To generate skewed data, with -15% and +80% as 95% confidence:
+#skew = 1000.
+#loc=-0.38
+#upperU = 0.8
+
+### To generate skewed data, with -40% and +80% as 95% confidence:
+#skew=1.
+#loc=0.
+#upperU = 0.8
+
+### To generate skewed data, with -15% and +40% as 95% confidence:
+skew = 1.25
+loc=0.
+upperU = 0.4
+#
+upper95=skewnorm.interval(0.95,skew,loc=loc)[1]
+lower95=skewnorm.interval(0.95,skew,loc=loc)[0]
+
+lowerU = upperU*lower95/upper95
+
+print('Upper percent error = '+str(upperU*100)+r'%, lower percent error = '+str(lowerU*100)+r'%.')
+    
+STD_CH2_CH2 = upperU * np.max(U_TraPPE_CH2_CH2) / upper95
+STD_CH2_CH = upperU * np.max(U_TraPPE_CH2_CH) / upper95
+STD_CH2_C = upperU * np.max(U_TraPPE_CH2_C) / upper95
+STD_CH_CH = STD_CH2_CH
+                        
+MCMC_CH2_CH2 = skewnorm.rvs(skew,loc=loc*STD_CH2_CH2,scale=STD_CH2_CH2,size=N_MCMC)
+MCMC_CH2_CH = skewnorm.rvs(skew,loc=loc*STD_CH2_CH,scale=STD_CH2_CH,size=N_MCMC)
+MCMC_CH2_C = skewnorm.rvs(skew,loc=loc*STD_CH2_C,scale=STD_CH2_C,size=N_MCMC)   
+MCMC_CH_CH = skewnorm.rvs(skew,loc=loc*STD_CH_CH,scale=STD_CH_CH,size=N_MCMC)
+
+plt.hist(MCMC_CH2_CH2,bins=50,color='k',alpha=0.2)
+plt.hist(MCMC_CH2_CH,bins=50,color='r',alpha=0.2)
+plt.hist(MCMC_CH2_C,bins=50,color='b',alpha=0.2)
 plt.show()
+
+#skew_range = np.linspace(0,50,50)
+#loc_range = np.linspace(-10,0,50)
+#
+#lower95 = np.zeros([len(skew_range),len(loc_range)])
+#upper95 = np.zeros([len(skew_range),len(loc_range)])
+#skewmean = np.zeros([len(skew_range),len(loc_range)])
+#
+#for iskew, skew in enumerate(skew_range):
+#    for iloc, loc in enumerate(loc_range):
+#
+#        skewnorm_interval = skewnorm.interval(0.95,skew,loc=loc)
+#        skewnorm_mean = skewnorm.mean(skew,loc=loc)
+#        
+#        lower95[iskew,iloc] = skewnorm_interval[0]
+#        upper95[iskew,iloc] = skewnorm_interval[1]
+#        skewmean[iskew,iloc] = skewnorm_mean
+#        
+##        if skewnorm_mean < 0.1 and skewnorm_mean > -0.1:
+##            
+##            print('skew = '+str(skew)+', loc = '+str(loc))
+##            print(skewnorm.interval(0.95,skew,loc=loc))
+##            print(skewnorm.mean(skew,loc=loc))
+##            
+##            if skewnorm_interval[0] < -1.5 and skewnorm_interval[1] > 4:
+##                
+##                print('skew = '+str(skew)+', loc = '+str(loc))
+##                print(skewnorm.interval(0.95,skew,loc=loc))
+##                print(skewnorm.mean(skew,loc=loc))
+#
+#fig = plt.figure(figsize=[6,6])
+#CS = plt.contour(loc_range,skew_range,skewmean)
+#cbar = fig.colorbar(CS)
+#cbar.add_lines(CS)
+#plt.title('Mean')
+#plt.xlabel('loc')
+#plt.ylabel('skew')
+#plt.show()
+#
+#fig = plt.figure(figsize=[6,6])
+#CS = plt.contour(loc_range,skew_range,lower95)
+#cbar = fig.colorbar(CS)
+#cbar.add_lines(CS)
+#plt.title('Lower 95')
+#plt.xlabel('loc')
+#plt.ylabel('skew')
+#plt.show()
+#
+#fig = plt.figure(figsize=[6,6])
+#CS = plt.contour(loc_range,skew_range,upper95)
+#cbar = fig.colorbar(CS)
+#cbar.add_lines(CS)
+#plt.title('Upper 95')
+#plt.xlabel('loc')
+#plt.ylabel('skew')
+#plt.show()
 
 fig = plt.figure(figsize=[8,8])
 
@@ -243,38 +348,47 @@ print(build_GROMACS_tors(a_TraPPE_CH2_C))
 GROMACS_MCMC_CH2_CH2 = np.zeros([len(MCMC_CH2_CH2),6])
 GROMACS_MCMC_CH2_CH = np.zeros([len(MCMC_CH2_CH),6])
 GROMACS_MCMC_CH2_C = np.zeros([len(MCMC_CH2_C),6])
+GROMACS_MCMC_CH_CH = np.zeros([len(MCMC_CH_CH),6])
 
-for ijk, (MCMC_i, MCMC_j, MCMC_k) in enumerate(zip(MCMC_CH2_CH2,MCMC_CH2_CH,MCMC_CH2_C)):
+for ijkl, (MCMC_i, MCMC_j, MCMC_k, MCMC_l) in enumerate(zip(MCMC_CH2_CH2,MCMC_CH2_CH,MCMC_CH2_C,MCMC_CH_CH)):
     
     a_MCMC_CH2_CH2 = a_TraPPE_CH2_CH2 + np.array([-MCMC_i,0.,0.,MCMC_i/2.])
     a_MCMC_CH2_CH = a_TraPPE_CH2_CH + np.array([-MCMC_j,0.,0.,MCMC_j/2.])
     a_MCMC_CH2_C = a_TraPPE_CH2_C + np.array([-MCMC_k,0.,0.,MCMC_k/2.])
-
-    GROMACS_MCMC_CH2_CH2[ijk,:] = build_GROMACS_tors(a_MCMC_CH2_CH2)
-    GROMACS_MCMC_CH2_CH[ijk,:] = build_GROMACS_tors(a_MCMC_CH2_CH)
-    GROMACS_MCMC_CH2_C[ijk,:] = build_GROMACS_tors(a_MCMC_CH2_C)
+    a_MCMC_CH_CH = a_TraPPE_CH_CH + np.array([-MCMC_l,0.,0.,MCMC_l/2.])
+    
+    GROMACS_MCMC_CH2_CH2[ijkl,:] = build_GROMACS_tors(a_MCMC_CH2_CH2)
+    GROMACS_MCMC_CH2_CH[ijkl,:] = build_GROMACS_tors(a_MCMC_CH2_CH)
+    GROMACS_MCMC_CH2_C[ijkl,:] = build_GROMACS_tors(a_MCMC_CH2_C)
+    GROMACS_MCMC_CH_CH[ijkl,:] = build_GROMACS_tors(a_MCMC_CH_CH)
 
 f = open('MCMC_CH2_CH2_tors','w')
-f.write('C0'+'\t'+'C1'+'\t'+'C2'+'\t'+'C3'+'\t'+'C4'+'\t'+'C5'+'\n')
+#f.write('C0'+'\t'+'C1'+'\t'+'C2'+'\t'+'C3'+'\t'+'C4'+'\t'+'C5'+'\n')
 
 g = open('MCMC_CH2_CH_tors','w')
-g.write('C0'+'\t'+'C1'+'\t'+'C2'+'\t'+'C3'+'\t'+'C4'+'\t'+'C5'+'\n')
+#g.write('C0'+'\t'+'C1'+'\t'+'C2'+'\t'+'C3'+'\t'+'C4'+'\t'+'C5'+'\n')
 
 h = open('MCMC_CH2_C_tors','w')
-h.write('C0'+'\t'+'C1'+'\t'+'C2'+'\t'+'C3'+'\t'+'C4'+'\t'+'C5'+'\n')
+#h.write('C0'+'\t'+'C1'+'\t'+'C2'+'\t'+'C3'+'\t'+'C4'+'\t'+'C5'+'\n')
 
-for MCMC_f,MCMC_g,MCMC_h in zip(GROMACS_MCMC_CH2_CH2,GROMACS_MCMC_CH2_CH,GROMACS_MCMC_CH2_C):
+i = open('MCMC_CH_CH_tors','w')
+#i.write('C0'+'\t'+'C1'+'\t'+'C2'+'\t'+'C3'+'\t'+'C4'+'\t'+'C5'+'\n')
+
+for MCMC_f,MCMC_g,MCMC_h,MCMC_i in zip(GROMACS_MCMC_CH2_CH2,GROMACS_MCMC_CH2_CH,GROMACS_MCMC_CH2_C,GROMACS_MCMC_CH_CH):
         
-    for C_fi, C_gi, C_hi in zip(MCMC_f,MCMC_g,MCMC_h):
+    for C_fj, C_gj, C_hj, C_ij in zip(MCMC_f,MCMC_g,MCMC_h,MCMC_i):
         
-        f.write(str(C_fi)+'\t')
-        g.write(str(C_gi)+'\t')
-        h.write(str(C_hi)+'\t')
+        f.write(str(C_fj)+'\t')
+        g.write(str(C_gj)+'\t')
+        h.write(str(C_hj)+'\t')
+        i.write(str(C_ij)+'\t')
                 
     f.write('\n')
     g.write('\n')
     h.write('\n')
+    i.write('\n')    
     
 f.close()
 g.close()
 h.close()
+i.close()
